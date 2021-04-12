9e5f08d482ec4065fb3855218b489ec68c4e35c3
Benoit Sepe <benoit.sepe@gmail.com>
2021-04-12 14:47:38 +0200

    Revert "Default columns are created when creating a sprint"
    This reverts commit d90ac2adf01a24c5f5a8af4cbdd7f7d176ff551e.

--- src/state/reducers/sprintReducers.test.ts
+++ src/state/reducers/sprintReducers.test.ts
@@ -1,5 +1,5 @@
 import { v4 as uuidv4 } from "uuid";
-import { Column, ProjectData } from "../../types/types";
+import { ProjectData } from "../../types/types";
 import { INITIAL_PROJECT_DATA } from "../constants";
 import { closeSprint, createSprint } from "./sprintReducers";
 
@@ -18,17 +18,7 @@ describe("createSprint", () => {
 
     const actualResult = createSprint({ projectData, id, goal, startDate, endDate, isOpen: true });
 
-    const actualResultWithoutColumns: ProjectData = {
-      ...actualResult,
-      sprints: {
-        ...actualResult.sprints,
-        [id]: {
-          ...actualResult.sprints[id],
-          data: {},
-        },
-      },
-    };
-    const expectedResultWithoutColumns: ProjectData = {
+    const expectedResult: ProjectData = {
       ...projectData,
       sprints: {
         ...projectData.sprints,
@@ -42,27 +32,8 @@ describe("createSprint", () => {
         },
       },
     };
-    expect(actualResultWithoutColumns).toStrictEqual(expectedResultWithoutColumns);
 
-    const actualColumns: Column[] = Object.values(actualResult.sprints[id].data);
-    const expectedColumns: Column[] = [
-      {
-        items: {},
-        list_title: "Sprint Backlog",
-        position: 0,
-      },
-      {
-        items: {},
-        list_title: "Doing",
-        position: 1,
-      },
-      {
-        items: {},
-        list_title: "Done",
-        position: 2,
-      },
-    ];
-    expect(actualColumns).toStrictEqual(expectedColumns);
+    expect(actualResult).toStrictEqual(expectedResult);
   });
   test("it should create a sprint without altering the other objects", () => {
     const projectData = INITIAL_PROJECT_DATA;
@@ -75,17 +46,7 @@ describe("createSprint", () => {
 
     const actualResult = createSprint({ projectData, id, goal, startDate, endDate, isOpen });
 
-    const actualResultWithoutColumns: ProjectData = {
-      ...actualResult,
-      sprints: {
-        ...actualResult.sprints,
-        [id]: {
-          ...actualResult.sprints[id],
-          data: {},
-        },
-      },
-    };
-    const expectedResultWithoutColumns: ProjectData = {
+    const expectedResult: ProjectData = {
       ...projectData,
       sprints: {
         ...projectData.sprints,
@@ -99,27 +60,8 @@ describe("createSprint", () => {
         },
       },
     };
-    expect(actualResultWithoutColumns).toStrictEqual(expectedResultWithoutColumns);
 
-    const actualColumns: Column[] = Object.values(actualResult.sprints[id].data);
-    const expectedColumns: Column[] = [
-      {
-        items: {},
-        list_title: "Sprint Backlog",
-        position: 0,
-      },
-      {
-        items: {},
-        list_title: "Doing",
-        position: 1,
-      },
-      {
-        items: {},
-        list_title: "Done",
-        position: 2,
-      },
-    ];
-    expect(actualColumns).toStrictEqual(expectedColumns);
+    expect(actualResult).toStrictEqual(expectedResult);
   });
 
   test("it should throw an error if startDate > endDate", () => {
--- src/state/reducers/sprintReducers.ts
+++ src/state/reducers/sprintReducers.ts
@@ -1,5 +1,4 @@
-import { v4 as uuidv4 } from "uuid";
-
+/* eslint-disable import/prefer-default-export */
 import { ProjectData } from "../../types/types";
 import { someItemsNotDone } from "./closeSprintUtils";
 
@@ -21,34 +20,13 @@ export const createSprint = ({
   if (startDate > endDate) {
     throw new Error("End date must be after start date");
   }
-
-  const idSprintBacklog = uuidv4();
-  const idDoing = uuidv4();
-  const idDone = uuidv4();
-
   const position = Object.keys(projectData.sprints).length;
   const newProjectData: ProjectData = {
     ...projectData,
     sprints: {
       ...projectData.sprints,
       [id]: {
-        data: {
-          [idSprintBacklog]: {
-            items: {},
-            list_title: "Sprint Backlog",
-            position: 0,
-          },
-          [idDoing]: {
-            items: {},
-            list_title: "Doing",
-            position: 1,
-          },
-          [idDone]: {
-            items: {},
-            list_title: "Done",
-            position: 2,
-          },
-        },
+        data: {},
         goal,
         startDate,
         endDate,
